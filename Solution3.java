3b3c198aee985f73ff41dd9b2e8773e1ec75d88a
陈英欢 <1010428527@qq.com>
2020-06-04 20:26:10 +0800

    1295

---
+++ Solution3.java
@@ -0,0 +1,13 @@
+class Solution3 {
+    public int findNumbers(int[] nums) {
+
+        int res=0;
+        for(int i:nums){
+            if(String.valueOf(i).length()%2==0){
+                res++;
+            }
+        }
+
+        return res;
+    }
+}
